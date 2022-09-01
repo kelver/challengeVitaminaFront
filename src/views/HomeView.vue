f9ee7648f0396b7de84a5d7431240aeead02659f
Kelver <kelver_kmv@hotmail.com>
2022-09-01 19:24:51 -0300

    Feat: Home component and components base composition's

---
+++ src/views/HomeView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex flex-col min-h-screen">
+        <Header />
+
+        <main class="">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <!-- Replace with your content -->
+                <div class="px-4 py-6 sm:px-0">
+                    <div class="rounded-lg h-auto">
+
+                    </div>
+                </div>
+                <!-- /End replace -->
+            </div>
+        </main>
+
+        <Footer />
+    </div>
+
+</template>
+
+<script>
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+export default {
+    name: 'HomeView',
+    components: {Header, Footer},
+}
+</script>
+
+<style>
+body {
+    overflow: hidden;
+}
+</style>
